6ff9efffbfb2af55d99dd3664683a167c88517db
Roberto Requejo Fernández <roberto.requejo.fdz@gmail.com>
2025-04-08 11:37:01 -0600

    Update mrp_production.py

--- mrp_embalaje_especificacion/models/mrp_production.py
+++ mrp_embalaje_especificacion/models/mrp_production.py
@@ -1,9 +1,12 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
-    @api.model
+    cantidad_embalaje = fields.Float(string='Cantidad Embalaje')
+
+    @api.model 
     def create(self, vals):
         origin = vals.get('origin')
         if origin:
@@ -12,16 +15,16 @@ class MrpProduction(models.Model):
                 # Get all unique product_packaging_id values, ignoring empty ones
                 packaging_records = sale_order.order_line.mapped('product_packaging_id').filtered(lambda p: p.id)
                 unique_packaging_ids = list(set(packaging_records.ids))
-
+                vals['cantidad_embalaje'] = packaging_records[0].qty
                 if len(unique_packaging_ids) == 1 and packaging_records:
                     # Case: All packaging IDs are the same
-                    total_qty = sum(sale_order.order_line.mapped('product_packaging_qty'))
+                    total_qty = sum(sale_order.order_line.mapped('product_uom_qty'))
                     packaging_name = packaging_records[0].display_name if packaging_records else ''
                     vals['tarimas'] = f"{int(total_qty)} {packaging_name}"
                     vals['tarimas_iguales'] = True
                 else:
                     # Case: Different packaging IDs
-                    total_qty = sum(sale_order.order_line.mapped('product_packaging_qty'))
+                    total_qty = sum(sale_order.order_line.mapped('product_uom_qty'))
                     vals['tarimas'] = f"{int(total_qty)}"
                     vals['tarimas_iguales'] = False
                     detalles_list = []
@@ -32,9 +35,9 @@ class MrpProduction(models.Model):
                         packaging = line.product_packaging_id
                         if packaging:
                             if packaging in packaging_qty:
-                                packaging_qty[packaging] += line.product_packaging_qty
+                                packaging_qty[packaging] += line.product_uom_qty
                             else:
-                                packaging_qty[packaging] = line.product_packaging_qty
+                                packaging_qty[packaging] = line.product_uom_qty
 
                     # Create a detail for each packaging with its calculated quantity
                     for packaging, qty in packaging_qty.items():
@@ -45,3 +48,11 @@ class MrpProduction(models.Model):
                     vals['detalles_tarimas'] = detalles_list
 
         return super(MrpProduction, self).create(vals)
+    
+    @api.constrains('state')
+    def _check_state_and_update_product_qty(self):
+        for rec in self:
+            # raise UserError(rec.cantidad_embalaje)
+            if rec.state == 'confirmed':
+                rec.qty_producing = rec.cantidad_embalaje
+                
